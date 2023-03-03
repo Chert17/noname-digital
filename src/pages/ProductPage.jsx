febd6ffe3828f103f6fb589d9b39f75f935eb007
Andrii Chertkov <andriichertkov@gmail.com>
2023-03-03 07:37:53 +0200

    add modal info for product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,25 +1,40 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ImageSlider from 'components/ui/slider/ImageSlider';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import { productItemSelector } from 'redux/products-slice/products-slice';
 import { getProductByIdThunk } from 'redux/products-slice/products-thunk';
 import { addProductToCast } from 'redux/cast-slice/cast-slice';
+import { tokenSelector } from 'redux/auth-slice/auth-slice';
+import { Modal } from 'components/ui/modal/Modal';
+import ModalInfo from 'components/ui/modal-info/ModalInfo';
 
 const ProductPage = () => {
   const { productId } = useParams();
   const location = useLocation();
   const path = location.state?.from || '/';
-
+  const token = useSelector(tokenSelector);
   const dispatch = useDispatch();
   const product = useSelector(productItemSelector) ?? {};
   const { title, brand, description, images, price } = product;
 
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     dispatch(getProductByIdThunk(productId));
   }, [dispatch, productId]);
+
+  const onModal = () => {
+    setShowModal(true);
+    if (showModal) return setShowModal(false);
+  };
+
+  const addProduct = () => {
+    if (!token) setShowModal(true);
+    else dispatch(addProductToCast({ ...product, quantity: 1 }));
+  };
 
   return (
     <Container maxWidth="xl">
@@ -39,17 +54,16 @@ const ProductPage = () => {
             {description}
           </Typography>
           <Typography gutterBottom>Price: {price}</Typography>
-          <Button
-            onClick={() =>
-              dispatch(addProductToCast({ ...product, quantity: 1 }))
-            }
-            size="large"
-            variant="outlined"
-          >
+          <Button onClick={addProduct} size="large" variant="outlined">
             <AddShoppingCartIcon />
           </Button>
         </Box>
       </Box>
+      {showModal && (
+        <Modal onClose={onModal}>
+          <ModalInfo onClose={onModal} />
+        </Modal>
+      )}
     </Container>
   );
 };
